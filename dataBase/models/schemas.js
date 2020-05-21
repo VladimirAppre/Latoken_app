ed712b964c4515137b99d7e58ce5e236d72e63d1
Vladimir <vladimir.appre@gmail.com>
2020-05-21 11:21:46 +0300

    add user

--- dataBase/models/schemas.js
+++ dataBase/models/schemas.js
@@ -13,15 +13,20 @@ const personSchema = new mongoose.Schema({
   deviationInMinutes: Schema.Types.Mixed,
   createdAt: { type: Date },
 });
-
 const Person = mongoose.model("Person", personSchema);
 
-// userSchema.statics.mostRecent = async function() {
-//     return this.find().sort('createdAt').limit(5).exec();
-// }
+const userSchema = new mongoose.Schema({
+  nick: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+const User = mongoose.model("User", userSchema);
 
 // userSchema.statics.mostRecent = async function() {
 //     return this.find().sort('createdAt').limit(5).exec();
 // }
 
-module.exports = { Person };
+// userSchema.statics.mostRecent = async function() {
+//     return this.find().sort('createdAt').limit(5).exec();
+// }
+
+module.exports = { Person, User };
