fe6b832f0b84cd90977801c555bc78dc862386af
Vladimir <vladimir.appre@gmail.com>
2020-05-21 12:48:22 +0300

    add check isNumber

--- dataBase/parsing/parsing.js
+++ dataBase/parsing/parsing.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const mongoose = require("mongoose");
 const { Person } = require("../models/schemas");
-require("dotenv").config;
+// require("dotenv").config;
 
-mongoose.connect(`mongodb+srv://mikhail:${process.env.mikhail_password}@cluster0-hjq5d.mongodb.net/test?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://mikhail:elbrus@cluster0-hjq5d.mongodb.net/test?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 //mongodb://localhost:27017/latoken
-
+// mongodb+srv://mikhail:${process.env.mikhail_password}@cluster0-hjq5d.mongodb.net/test?retryWrites=true&w=majority
 async function parserData(path) {
   let file = fs.readFileSync(path, "utf8");
   function csvToArray(text) {
@@ -33,9 +33,7 @@ async function parserData(path) {
     }
     return ret;
   }
-  let clearFile = csvToArray(file);
-
-  //  let regexp = /(?:^"|,")(""|[\w\W]*?)(?=",|"$)|(?:^(?!")|,(?!"))([^,]*?)(?=$|,)|(\r\n|\n)/g;
+  let clearFile = csvToArray(file);  //  let regexp = /(?:^"|,")(""|[\w\W]*?)(?=",|"$)|(?:^(?!")|,(?!"))([^,]*?)(?=$|,)|(\r\n|\n)/g;
 
   for (let i = 2; i < clearFile.length - 1; i++) {
     const dataPerson = clearFile[i];
@@ -47,11 +45,11 @@ async function parserData(path) {
         email: dataPerson[3],
         role: dataPerson[4],
         task: dataPerson[5],
-        frequencyDoneYesterday: dataPerson[6],
-        frequencyPerWeekPlan: dataPerson[7],
-        timePerTaskMinutes: dataPerson[8],
-        timePerformed: dataPerson[9],
-        deviationInMinutes: dataPerson[10],
+        frequencyDoneYesterday: (!isNaN(dataPerson[6]))? +dataPerson[6] : dataPerson[6],
+        frequencyPerWeekPlan: (!isNaN(dataPerson[7]))? +dataPerson[7] : dataPerson[7],
+        timePerTaskMinutes: (!isNaN(dataPerson[8]))? +dataPerson[8] : dataPerson[8],
+        timePerformed: (!isNaN(dataPerson[9]))? +dataPerson[9] : dataPerson[9],
+        deviationInMinutes: +dataPerson[10],
         createdAt: Date.now(),
       });
       await person.save();
@@ -60,3 +58,5 @@ async function parserData(path) {
 }
 
 parserData("../csv/Test task 2 - Person performance.csv");
+
+module.exports = {parserData};
