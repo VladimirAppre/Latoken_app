9f314b9b0f34e664e11aa4de852a2f917531d0eb
Vladimir <vladimir.appre@gmail.com>
2020-05-21 10:33:01 +0300

    add database

--- dataBase/parsing/parsing.js
+++ dataBase/parsing/parsing.js
@@ -1,39 +1,47 @@
-const fs = require('fs');
+const fs = require("fs");
 const mongoose = require("mongoose");
-const { Person } = require('../models/schemas');
+const { Person } = require("../models/schemas");
+require("dotenv").config;
 
-mongoose.connect(
-  'mongodb://localhost:27017/latoken',
-  {
-    useNewUrlParser: true, useUnifiedTopology: true
-  });
+mongoose.connect(`mongodb+srv://mikhail:${process.env.mikhail_password}@cluster0-hjq5d.mongodb.net/test?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//mongodb://localhost:27017/latoken
 
-async function parserData(path){
-  let file = fs.readFileSync(path, 'utf8')
+async function parserData(path) {
+  let file = fs.readFileSync(path, "utf8");
   function csvToArray(text) {
-    let p = '', row = [''], ret = [row], i = 0, r = 0, s = !0, l;
+    let p = "",
+      row = [""],
+      ret = [row],
+      i = 0,
+      r = 0,
+      s = !0,
+      l;
     for (l of text) {
-        if ('"' === l) {
-            if (s && l === p) row[i] += l;
-            s = !s;
-        } else if (',' === l && s) l = row[++i] = '';
-        else if ('\n' === l && s) {
-            if ('\r' === p) row[i] = row[i].slice(0, -1);
-            row = ret[++r] = [l = '']; i = 0;
-        } else row[i] += l;
-        p = l;    
+      if ('"' === l) {
+        if (s && l === p) row[i] += l;
+        s = !s;
+      } else if ("," === l && s) l = row[++i] = "";
+      else if ("\n" === l && s) {
+        if ("\r" === p) row[i] = row[i].slice(0, -1);
+        row = ret[++r] = [(l = "")];
+        i = 0;
+      } else row[i] += l;
+      p = l;
     }
     return ret;
-  };
-  let clearFile = csvToArray(file)
+  }
+  let clearFile = csvToArray(file);
 
   //  let regexp = /(?:^"|,")(""|[\w\W]*?)(?=",|"$)|(?:^(?!")|,(?!"))([^,]*?)(?=$|,)|(\r\n|\n)/g;
 
-  for (let i = 1; i < clearFile.length - 1; i++) {
-     const dataPerson =  clearFile[i]
+  for (let i = 2; i < clearFile.length - 1; i++) {
+    const dataPerson = clearFile[i];
     const email = dataPerson[3];
-    
-    if(typeof email === 'string') {
+
+    if (typeof email === "string" && email.length > 3) {
       const person = new Person({
         departament: dataPerson[0],
         email: dataPerson[3],
@@ -45,13 +53,10 @@ async function parserData(path){
         timePerformed: dataPerson[9],
         deviationInMinutes: dataPerson[10],
         createdAt: Date.now(),
-      })      
-      await person.save()
+      });
+      await person.save();
     }
   }
 }
 
-parserData('../csv/Test task 2 - Person performance.csv');
-
-
-
+parserData("../csv/Test task 2 - Person performance.csv");
